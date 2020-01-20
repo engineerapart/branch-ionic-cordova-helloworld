3b00a8bf2f59080aef03d923c5f7b8657f831935
codinronan <patrick@engineerapart.com>
2020-01-20 16:31:17 -0600

    feat(branch): add branch plugin & init code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,6 +22,19 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      this.initializeBranch();
+    });
+  }
+
+  initializeBranch() {
+    // Branch initialization
+    (window as any).Branch.initSession().then((data: any) => {
+      if (data['+clicked_branch_link']) {
+        // read deep link data on click
+        alert('Deep Link Data: ' + JSON.stringify(data));
+      } else {
+        alert('Branch initialized without any data');
+      }
     });
   }
 }
